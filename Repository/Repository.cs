739e4c6f5742aeb5ae51de7dabfcf1861f9b7800
Jose Camarena <jose.j.camarena@gmail.com>
2024-03-10 08:53:42 -0700

    Update Repository return type

---
+++ Repository/Repository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Repository.IRepository;
+
+namespace WebApplication1.Repository
+{
+    public class Repository<T> :  IRepository<T> where T : class
+    {
+        private readonly ApplicationDBContext _context;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDBContext context)
+        {
+            _context = context;
+            this.dbSet = _context.Set<T>();
+        }
+        public ICollection<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+    }
+}
